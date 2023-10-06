8871d8b4f211241faeeaf3ebe173b59add2742a9
andryushchenkok <andryushchenko.k@yandex.ru>
2023-10-06 20:01:16 +0300

    Линейный поиск в массиве

---
+++ src/algos/Search.java
@@ -0,0 +1,14 @@
+package algos;
+
+public class Search {
+
+    public static int LinearSearch(int[] array, int target) {
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
