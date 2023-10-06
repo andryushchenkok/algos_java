46243b3065ab815af6d9d0e0041256dfa82b5ea7
andryushchenkok <andryushchenko.k@yandex.ru>
2023-10-06 20:11:15 +0300

    Линейный поиск в списке

--- src/algos/Search.java
+++ src/algos/Search.java
@@ -1,11 +1,23 @@
 package algos;
 
+import java.util.List;
+
 public class Search {
 
     public static int LinearSearch(int[] array, int target) {
 
         for (int i = 0; i < array.length; i++) {
             if (array[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static <T> int LinearSearch(List<T> list, T target) {
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) == target) {
                 return i;
             }
         }
